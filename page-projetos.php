f41217150786b796ece7e84a807add27cc2ea92b
Thavi <thavilang@gmail.com>
2024-02-29 19:28:15 -0300

    atualizando

---
+++ page-projetos.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="container">
+        <h1 class="hidden-text"><?php the_title(); ?></h1>
+    </div>
+</main>
+
+<?php get_footer(); ?>
--- single-texto.php
+++ single-texto.php
@@ -5,7 +5,7 @@
     <div class="container">
         <a class="breadcrumb" href="https://renatapelegrini.thavi.dev/textos/">
             <svg width="6" height="13" viewBox="0 0 6 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M-2.84124e-07 6.5L6 0.870834L6 12.1292L-2.84124e-07 6.5Z" fill="#B6B6B6"/>
+                <path d="M-2.84124e-07 6.5L6 0.870834L6 12.1292L-2.84124e-07 6.5Z" fill="#B6B6B6" />
             </svg>
             <span>Textos</span>
         </a>
@@ -14,7 +14,9 @@
             <p class="txtupper fw400"><?php echo get_field('autor', $post->ID) ?></p>
         </div>
         <div class="content">
-            <?php echo get_field('conteudo', $post->ID) ?>
+            <div class="editor">
+                <?php echo get_field('conteudo', $post->ID) ?>
+            </div>
         </div>
     </div>
 </main>
