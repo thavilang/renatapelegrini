5b0fc825ea9366032b46ae8f455cfbd85cc95554
BrunoFigueiredo <bruno.figueiredo@agenciakindle.com.br>
2024-03-01 15:21:26 -0300

    pag projetos

--- page-projetos.php
+++ page-projetos.php
@@ -1,8 +1,60 @@
 <?php get_header(); ?>
 
-<main>
+<main id="projetos">
     <div class="container">
         <h1 class="hidden-text"><?php the_title(); ?></h1>
+
+        <div class="grid model-1">
+            <h2 class="txtupper fs32 fw400">AÇÕES colaborativas</h2>
+            <div class="bg-grid">
+                <div class="grid-scroll">
+                    <div class="box">
+                        <span class="txtupper fw400">OCEANUNI 2023</span>
+                    </div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                </div>
+            </div>
+        </div>
+        <div class="grid model-2">
+            <h2 class="txtupper fs32 fw400">Blog</h2>
+            <div class="bg-grid">
+                <div class="grid-scroll">
+                    <div class="box">
+                        <span class="txtupper fw400">TITULO</span>
+                    </div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                </div>
+            </div>
+        </div>
+        <div class="grid model-3">
+            <h2 class="txtupper fs32 fw400">AÇÕES colaborativas</h2>
+            <div class="bg-grid">
+                <div class="grid-scroll">
+                    <div class="box">
+                        <span class="txtupper fw400">OCEANUNI 2023</span>
+                    </div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
--- single-trabalho.php
+++ single-trabalho.php
@@ -6,7 +6,7 @@
 <main id="trabalho">
     <h1 class="hidden-text"><?php echo get_the_title($post->ID) ?></h1>
     <div class="container">
-        <a class="breadcrumb" href="https://renatapelegrini.thavi.dev/textos/">
+        <a class="breadcrumb" href="https://renatapelegrini.thavi.dev/trabalhos/">
             <svg width="6" height="13" viewBox="0 0 6 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M-2.84124e-07 6.5L6 0.870834L6 12.1292L-2.84124e-07 6.5Z" fill="#B6B6B6"/>
             </svg>
