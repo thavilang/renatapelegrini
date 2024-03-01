71c5922e09fe3d8382fe57ec3b17bf6fc034a194
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2024-03-01 20:12:23 -0300

    Update page-trabalhos.php

--- page-trabalhos.php
+++ page-trabalhos.php
@@ -9,19 +9,21 @@ get_header();
 <main class="trabalhos">
     <h1 class="hidden-text"><?php echo get_the_title(); ?></h1>
     <div class="container">
-        <div class="box-mansory ajaxScrooling" data-posts_per_page="6" data-post_type="trabalho" data-post_status="publish" data-element_item="post-item">
-            <?php
-            $args = array( 
-                'posts_per_page'   => 6,
-                'post_type'        => 'trabalho',
-                'post_status'      => 'publish',
-            );
-            $textos = get_posts($args);
-            foreach ($textos as $post) {
-                include 'elements/post-item.php';
-            } // foreach ($textos as $post)
-            wp_reset_postdata();
-            ?>
+        <div class="ajaxScrooling" data-posts_per_page="6" data-post_type="trabalho" data-post_status="publish" data-element_item="post-item">
+            <div class="box-mansory content">
+                <?php
+                $args = array(
+                    'posts_per_page'   => 6,
+                    'post_type'        => 'trabalho',
+                    'post_status'      => 'publish',
+                );
+                $textos = get_posts($args);
+                foreach ($textos as $post) {
+                    include 'elements/post-item.php';
+                } // foreach ($textos as $post)
+                wp_reset_postdata();
+                ?>
+            </div>
         </div>
     </div>
 </main>
