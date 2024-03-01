084fe62a5f5fcbfb760b722acd0200d76c5f96aa
Thavi <thavilang@gmail.com>
2024-03-01 19:19:21 -0300

    trabalhos

--- elements/post-item.php
+++ elements/post-item.php
@@ -1,4 +1,16 @@
-<a href="<?php echo get_the_permalink($post->ID); ?>" class="post">
-    <div><img src="<?php echo get_field('galeria', $post->ID)[0]['sizes']['thumbnail'] ?>" alt="<?php echo get_field('galeria', $post->ID)[0]['alt'] ?>"></div>
-    <h2><?php echo get_the_title($post->ID) ?></h2>
-</a>
+<?php
+$imagem = get_field('galeria', $post->ID)[0];
+$orientacaoImagem = $imagem['width'] > $imagem['height'] ? 'horizontal' : 'quadrado';
+$orientacaoImagem = $imagem['width'] < $imagem['height'] ? 'vertical' : $orientacaoImagem;
+?>
+<div class="post <?php echo $orientacaoImagem ?>" gsap-aparecer-fade>
+    <a href="<?php echo get_the_permalink($post->ID); ?>">
+        <div class="post__img"><img src="<?php echo $imagem['sizes']['thumbnail'] ?>" alt="<?php echo $imagem['alt'] ?>"></div>
+        <h2>
+            <?php if ($post->post_type == 'exposicao') { ?>            
+                [<?php echo get_field('status', $post->ID) ?>] 
+            <?php } // if $post->post_type == 'exposicao' ?>
+            <?php echo get_the_title($post->ID) ?>
+        </h2>
+    </a>
+</div>
