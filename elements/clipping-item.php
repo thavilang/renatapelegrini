112aa4c2cc65ceda841024c91b2ac32742224144
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2024-03-02 14:05:47 -0300

    feat fadein a

--- elements/clipping-item.php
+++ elements/clipping-item.php
@@ -6,8 +6,8 @@ $count = mb_strlen($titulo);
 $classe = $count < 30 ? 'pequeno' : 'medio';
 $classe = $count > 99 ? 'grande' : $classe; ?>
 
-<div class="<?php echo $classe ?> efeito-aparecer">
-    <a href="<?php echo get_field('link', $post->ID) ?>" target="_blank" gsap-aparecer-fade>
+<div class="<?php echo $classe ?> ">
+    <a class="efeito-aparecer" href="<?php echo get_field('link', $post->ID) ?>" target="_blank" gsap-aparecer-fade>
         <h2 class="fw400 fs24 txtupper"><?php echo $titulo ?></h2>
         <p><?php echo get_field('fonte', $post->ID) ?></p>
     </a>
